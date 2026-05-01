3d8c77667ccf8df794e51f5867d959c3a2615027
adminB9 <admin@b9tech.africa>
2026-05-01 08:44:21 +0000

    Production Stage Begins

--- 04-analytics_engineering/taxi_rides_ny/analyses/generate_staging_yaml.sql
+++ 04-analytics_engineering/taxi_rides_ny/analyses/generate_staging_yaml.sql
@@ -1,3 +1,3 @@
-dbt run-operation generate_model_yaml --args '{"model_names": ["stg_green_tripdata", "stg_yellow_tripdata"], "upstream_descriptions": true}'
+taxidbt run-operation generate_model_yaml --args '{"model_names": ["stg_green_tripdata", "stg_yellow_tripdata"], "upstream_descriptions": true}'
 
 dbt run-operation generate_model_yaml --args '{"model_names": ["dim_zones", "fact_trips", "dm_monthly_zone_revenue"], "upstream_descriptions": true}'
